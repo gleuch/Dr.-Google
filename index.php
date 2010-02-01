21cc9ed2ec938cff3318757f1f4fc7fca972462f
Greg Leuch <contact@gleuch.com>
2010-02-01 20:22:47 +0100

    fixing sub-dir path issue with second opinion file_get_contents URL.

--- index.php
+++ index.php
@@ -42,15 +42,15 @@ if ($route == 'search') {
     include('template/index.php');
 
   } elseif (isset($_GET['btnI'])) {
-    if (false) {
-      $urls = array(
-        'http://sanebride.files.wordpress.com/2009/07/sick-cat.jpg',
-      );
-      $index = rand(0, count($urls));
-      header('Location: '. $urls[$index]);
-      exit;
-
-    } else {
+    // if (false) {
+    //   $urls = array(
+    //     'http://sanebride.files.wordpress.com/2009/07/sick-cat.jpg',
+    //   );
+    //   $index = rand(0, count($urls));
+    //   header('Location: '. $urls[$index]);
+    //   exit;
+    // 
+    // } else {
       $base = str_replace('+', '_', trim($_GET['q']));
       if (is_file('tmp/'. $base .'.cache')) {
         echo file_get_contents('tmp/'. $base .'.cache');
@@ -63,12 +63,12 @@ if ($route == 'search') {
         file_put_contents('tmp/'. $base .'.cache', $content);
         ob_end_flush();
       }
-    }
+    // }
 
   } else {
     $find = array('/(src=[\'"]?)(\/)/i', '/(href=[\'"]?)(\/)/i', "/(\/\")/", "/http\:\/\/www\.google\.com\/images\/logo_sm\.gif/i", "/width=150 height=55/i", '/Google/');
     $replace = array('\1http://www.google.com\2', '\1http://www.google.com\2', '\1http://www.google.com', '/images/logo_sm.gif', "width=174 height=55", 'Dr. Google');
-    echo preg_replace($find, $replace, file_get_contents('http://www.google.com'. $_SERVER['REQUEST_URI']));
+    echo preg_replace($find, $replace, file_get_contents('http://www.google.com/'. str_replace(BASE_PATH, '', $_SERVER['REQUEST_URI'])));
   }
 
 } else if ($route == '/advanced_search') {
